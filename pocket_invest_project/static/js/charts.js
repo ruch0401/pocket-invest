30bcd862da1d367c861f1d4627c0a34d5d7a4418
Mahima Mandhana <mandhana.mahima@gmail.com>
2022-02-26 05:30:31 -0800

    adds quiz

--- pocket_invest_project/static/js/charts.js
+++ pocket_invest_project/static/js/charts.js
@@ -1,45 +1,45 @@
 Highcharts.chart('pie-chart', {
-  chart: {
-      type: 'pie',
-      options3d: {
-          enabled: true,
-          alpha: 45,
-          beta: 0
-      }
-  },
-  credits: {
-    enabled: false
-  },
-  title: {
-      text: '% of Money Blocked and Unlocked'
-  },
-  accessibility: {
-      point: {
-          valueSuffix: '%'
-      }
-  },
-  tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}% : ${point.y:1f}</b>'
-  },
-  plotOptions: {
-      pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          depth: 35,
-          dataLabels: {
-              enabled: true,
-              format: '{point.name}'
-          }
-      }
-  },
-  series: [{
-      type: 'pie',
-      name: 'Amount',
-      data: [
-          ['Money Blocked', 123220],
-          ['Money Unlocked', 463730]
-      ]
-  }]
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    credits: {
+        enabled: false
+    },
+    title: {
+        text: '% of Money Blocked and Unlocked'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}% : ${point.y:1f}</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Amount',
+        data: [
+            ['Money Blocked', 123220],
+            ['Money Unlocked', 463730]
+        ]
+    }]
 });
 
 Highcharts.chart('expense-chart', {
@@ -147,43 +147,18 @@ Highcharts.chart('bar-courses', {
 
     series: [
         {
-            name: "Browsers",
+            name: "Children",
             colorByPoint: true,
             data: [
                 {
-                    name: "Chrome",
+                    name: "Child1",
                     y: 62.74,
-                    drilldown: "Chrome"
+                    drilldown: "Child1"
                 },
                 {
-                    name: "Firefox",
+                    name: "Child2",
                     y: 10.57,
-                    drilldown: "Firefox"
-                },
-                {
-                    name: "Internet Explorer",
-                    y: 7.23,
-                    drilldown: "Internet Explorer"
-                },
-                {
-                    name: "Safari",
-                    y: 5.58,
-                    drilldown: "Safari"
-                },
-                {
-                    name: "Edge",
-                    y: 4.02,
-                    drilldown: "Edge"
-                },
-                {
-                    name: "Opera",
-                    y: 1.92,
-                    drilldown: "Opera"
-                },
-                {
-                    name: "Other",
-                    y: 7.62,
-                    drilldown: null
+                    drilldown: "Child2"
                 }
             ]
         }
@@ -196,218 +171,34 @@ Highcharts.chart('bar-courses', {
         },
         series: [
             {
-                name: "Chrome",
-                id: "Chrome",
+                name: "Child1",
+                id: "Child1",
                 data: [
                     [
-                        "v65.0",
+                        "Course1",
                         0.1
                     ],
                     [
-                        "v64.0",
+                        "Course2",
                         1.3
                     ],
                     [
-                        "v63.0",
+                        "Course3",
                         53.02
-                    ],
-                    [
-                        "v62.0",
-                        1.4
-                    ],
-                    [
-                        "v61.0",
-                        0.88
-                    ],
-                    [
-                        "v60.0",
-                        0.56
-                    ],
-                    [
-                        "v59.0",
-                        0.45
-                    ],
-                    [
-                        "v58.0",
-                        0.49
-                    ],
-                    [
-                        "v57.0",
-                        0.32
-                    ],
-                    [
-                        "v56.0",
-                        0.29
-                    ],
-                    [
-                        "v55.0",
-                        0.79
-                    ],
-                    [
-                        "v54.0",
-                        0.18
-                    ],
-                    [
-                        "v51.0",
-                        0.13
-                    ],
-                    [
-                        "v49.0",
-                        2.16
-                    ],
-                    [
-                        "v48.0",
-                        0.13
-                    ],
-                    [
-                        "v47.0",
-                        0.11
-                    ],
-                    [
-                        "v43.0",
-                        0.17
-                    ],
-                    [
-                        "v29.0",
-                        0.26
                     ]
                 ]
             },
             {
-                name: "Firefox",
-                id: "Firefox",
+                name: "Child2",
+                id: "Child2",
                 data: [
                     [
-                        "v58.0",
+                        "Course1",
                         1.02
                     ],
                     [
-                        "v57.0",
+                        "Course2",
                         7.36
-                    ],
-                    [
-                        "v56.0",
-                        0.35
-                    ],
-                    [
-                        "v55.0",
-                        0.11
-                    ],
-                    [
-                        "v54.0",
-                        0.1
-                    ],
-                    [
-                        "v52.0",
-                        0.95
-                    ],
-                    [
-                        "v51.0",
-                        0.15
-                    ],
-                    [
-                        "v50.0",
-                        0.1
-                    ],
-                    [
-                        "v48.0",
-                        0.31
-                    ],
-                    [
-                        "v47.0",
-                        0.12
-                    ]
-                ]
-            },
-            {
-                name: "Internet Explorer",
-                id: "Internet Explorer",
-                data: [
-                    [
-                        "v11.0",
-                        6.2
-                    ],
-                    [
-                        "v10.0",
-                        0.29
-                    ],
-                    [
-                        "v9.0",
-                        0.27
-                    ],
-                    [
-                        "v8.0",
-                        0.47
-                    ]
-                ]
-            },
-            {
-                name: "Safari",
-                id: "Safari",
-                data: [
-                    [
-                        "v11.0",
-                        3.39
-                    ],
-                    [
-                        "v10.1",
-                        0.96
-                    ],
-                    [
-                        "v10.0",
-                        0.36
-                    ],
-                    [
-                        "v9.1",
-                        0.54
-                    ],
-                    [
-                        "v9.0",
-                        0.13
-                    ],
-                    [
-                        "v5.1",
-                        0.2
-                    ]
-                ]
-            },
-            {
-                name: "Edge",
-                id: "Edge",
-                data: [
-                    [
-                        "v16",
-                        2.6
-                    ],
-                    [
-                        "v15",
-                        0.92
-                    ],
-                    [
-                        "v14",
-                        0.4
-                    ],
-                    [
-                        "v13",
-                        0.1
-                    ]
-                ]
-            },
-            {
-                name: "Opera",
-                id: "Opera",
-                data: [
-                    [
-                        "v50.0",
-                        0.96
-                    ],
-                    [
-                        "v49.0",
-                        0.82
-                    ],
-                    [
-                        "v12.1",
-                        0.14
                     ]
                 ]
             }
